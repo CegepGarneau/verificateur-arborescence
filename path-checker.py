0b42887d5e45df286b8682dff7613e6ceec5d428
Nicolas Richard <nicolas_richard77@hotmail.com>
2018-09-17 11:41:52 -0400

    checking files supposed to be in a student homework.

--- path-checker.py
+++ path-checker.py
@@ -3,14 +3,58 @@ import sys
 
 
 def validate(test, message):
+    """Displays a message if a test is false"""
     if (not test):
-        sys.stderr.write(message)
+        print("### " + message)
 
-def checkFile(path, message="Missing file"):
-    validate(not os.path.isfile(path), message + " : " + path)
+def checkFile(basepath, path, message="Missing file"):
+    """Checks whether a file exists and if not, displays an error message"""
+    validate(os.path.isfile(os.path.join(basePath, path)), message + " : " + path)
 
-def checkDir(path, message="Missing file"):
-    validate(not os.path.isdir(path), message + " : " + path)
+def checkDir(path, message="Missing directory"):
+    """Checks whether a directory exists and if not, displays an error message"""
+    validate(os.path.isdir(path), message + " : " + path)
+
+def checkDirectoryStructure(basePath):
+    """Checks the general directory structure"""
+    checkDir(basePath, "importations")
+
+    checkDir(basePath, "importations\\photos")
+    checkDir(basePath, "importations\\textes")
+    
+    checkDir(basePath, "recherches")
+
+    checkDir(basePath, "recherches\\images")
+    checkDir(basePath, "recherches\\images\\gif")
+    checkDir(basePath, "recherches\\images\\jpg")
+    checkDir(basePath, "recherches\\images\\png")
+    
+    checkDir(basePath, "recherches\\liens")
+    checkDir(basePath, "recherches\\réponses")
+
+def checkImportation(basePath):
+    """Checks files located in \\importations"""
+    checkFile(basePath, "Importations\\Photos\\1.jpg")
+    checkFile(basePath, "Importations\\Photos\\2.png")
+    checkFile(basePath, "Importations\\Photos\\4.gif")
+    
+    checkFile(basePath, "Importations\\textes\\3.txt")
+    checkFile(basePath, "Importations\\textes\\5.docx")
+
+def checkImages(basePath):
+    """Checks images located in Recherches\\images"""
+    checkFile(basePath, "Recherches\\Images\\gif\\athlète.gif")
+    checkFile(basePath, "Recherches\\Images\\jpg\\épreuve-olympique.jpg")
+    checkFile(basePath, "Recherches\\Images\\png\\passion.png")
+
+def checkLinks(basePath):
+    """Checks if a Word file is located in Recherches\\liens"""
+    checkFile(basePath, "Recherches\\liens\\liens.docx")
+
+def checkAnswers(basePath):
+    """Checks for Word files located in Recherches\\réponses"""
+    checkFile(basePath, "Recherches\\réponses\\recherche.docx")    
+    checkFile(basePath, "Recherches\\réponses\\réponses.docx")    
 
 
 if __name__ == "__main__":
@@ -23,6 +67,17 @@ if __name__ == "__main__":
         sys.stderr.write("The argument must be a directory : " + basePath)
         exit(2)
 
-    basePath = os.path.abspath(basePath)
+    print("DirectoryStructure")
+    checkDirectoryStructure(basePath)
+    
+    print("Importations")
+    checkImportation(basePath)
 
-    print("Working with " + basePath)
+    print("Images")
+    checkImages(basePath)
+
+    print("Liens")
+    checkLinks(basePath)
+
+    print("Réponses")
+    checkAnswers(basePath)
