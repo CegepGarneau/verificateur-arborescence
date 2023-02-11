3782efea46534c29f1a6e187bbb3d82a52798ff5
Nicolas Richard <nrichard@cegepgarneau.ca>
2023-02-11 10:48:39 -0500

    Amélioration du code

--- path-checker.py
+++ path-checker.py
@@ -1,13 +1,20 @@
+"""
+Permet de vérifier l'existance de fichiers et de dossier dans une arborescence,
+par exemple pour un travail pratique où l'on doit vérifier si une arborescence
+possède les bons fichiers et les bons répertoires, aux bons endroits.
+"""
+
 import re
 import os
 import sys
+import traceback
 import xml.dom.minidom as minidom
 
 
-def extractNameAndDa(path):
+def extract_name_and_da(path):
     """
     Extracts the name and DA from path generated from Omnivox's files
-    Ex : 
+    Ex :
     Noel-Parise_1944993_TP_1_-_Recherche_Int_Remis_le_2021-09-06_17h51m51s
     Yergeau-Rhéaume_2042114_TP_1_Remis_le_2021-09-09_13h37m17s
     """
@@ -16,59 +23,59 @@ def extractNameAndDa(path):
 
 def validate(test, message):
     """Displays a message if test is false"""
-    if (not test):
+    if not test:
         print("### " + message)
     return test
 
 
-def checkFile(dirpath, filename, message="Missing file"):
+def check_file(dirpath, filename, message="Missing file"):
     """Checks if a file exists and if not, displays an error message"""
     filepath = os.path.join(dirpath, filename)
     return validate(os.path.isfile(filepath), message + " : " + filepath)
 
 
-def checkDir(path, message="Missing directory"):
+def check_dir(path, message="Missing directory"):
     """Checks whether a directory exists and if not, displays an error message"""
     return validate(os.path.isdir(path), message + " : " + path)
 
 
-def checkDirectoryStructure(parentPath, currentNode):
+def check_directory_structure(parent_path, current_node):
     """Checks the general directory structure"""
-    type = currentNode.nodeName
+    arg_type = current_node.nodeName
 
-    if(type == "file"):
-        checkFile(parentPath, currentNode.getAttribute("name"))
-    elif(type == "dir"):
-        currentPath = os.path.join(
-            parentPath, currentNode.getAttribute("name"))
-        if(checkDir(currentPath)):
-            for child in currentNode.childNodes:
-                if (child.nodeType == minidom.Node.ELEMENT_NODE):
-                    checkDirectoryStructure(currentPath, child)
+    if arg_type == "file":
+        check_file(parent_path, current_node.getAttribute("name"))
+    elif arg_type == "dir":
+        current_path = os.path.join(
+            parent_path, current_node.getAttribute("name"))
+        if check_dir(current_path):
+            for child in current_node.childNodes:
+                if child.nodeType == minidom.Node.ELEMENT_NODE:
+                    check_directory_structure(current_path, child)
 
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 3):
-        sys.stderr.write("Config file and base directory missing")
+    if len(sys.argv) != 3:
+        sys.stderr.write("Two arguments needed : config file and base directory")
         exit(1)
 
-    configPath = sys.argv[1]
-    if (not os.path.isfile(configPath)):
-        sys.stderr.write("First argument must be a file : " + configPath)
+    config_path = sys.argv[1]
+    if not os.path.isfile(config_path):
+        sys.stderr.write("First argument must be a file : " + config_path)
         exit(2)
 
-    basePath = sys.argv[2]
-    if (not os.path.isdir(basePath)):
-        sys.stderr.write("Second argument must be a directory : " + basePath)
+    base_path = sys.argv[2]
+    if not os.path.isdir(base_path):
+        sys.stderr.write("Second argument must be a directory : " + base_path)
         exit(3)
 
-
-    print(extractNameAndDa(basePath))
+    print(extract_name_and_da(base_path))
 
     try:
-        with open(configPath) as configFile:
+        with open(config_path) as configFile:
             config = minidom.parse(configFile)
-        checkDirectoryStructure(basePath, config.documentElement)
+        check_directory_structure(base_path, config.documentElement)
     except Exception as ex:
-        sys.stderr.write("Error checking " + basePath, ex)
+        print("Error checking " + base_path, file=sys.stderr)
+        traceback.print_exc()
         exit(4)
